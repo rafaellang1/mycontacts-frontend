27da323d8cb168ae8ba0fbe0d3189fc98073c27a
Rafael Lang <rafael.lang9@gmail.com>
2023-08-24 23:49:08 -0400

    Criando compon. PageHeader / Criando compon. Input e Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,23 @@
+import PageHeader from '../../components/PageHeader';
+
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
-  return 'NewContactPage';
+  return (
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Facebook</option>
+        <option value="123">Twitter</option>
+        <option value="123">LinkedIn</option>
+
+      </Select>
+    </>
+  );
 }
