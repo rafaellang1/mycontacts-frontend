89c9c93e22dd1f060db78a466a064327aeda9bed
rafaellang1 <rafael.lang9@gmail.com>
2024-02-14 23:40:42 -0400

    melhorias de UI / UX: Exibindo mensagme de nenhum contrato encontrado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,14 @@ import {
 } from 'react';
 
 import {
-  Container, InputSearchContainer, Header, ListHeader, Card, ErrorContainer, EmptyListContainer,
+  Container,
+  InputSearchContainer,
+  Header,
+  ListHeader,
+  Card,
+  ErrorContainer,
+  EmptyListContainer,
+  SearchNotFoundContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -13,6 +20,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -147,6 +155,16 @@ export default function Home() {
             para cadastrar o seu primeiro contato!
           </p>
         </EmptyListContainer>
+        )}
+
+        {(contacts.length > 0 && filteredContacts.length < 1) && (
+          <SearchNotFoundContainer>
+            <img src={magnifierQuestion} alt="Magnifier question" />
+
+            <span>
+              Nenhum resultado foi encontrado para <strong>&quot;{searchTerm}&quot;</strong>
+            </span>
+          </SearchNotFoundContainer>
         )}
 
         {filteredContacts.length > 0 && (
