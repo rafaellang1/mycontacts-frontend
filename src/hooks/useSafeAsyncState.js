c852991ec364ff59e562fca6a6025ab021f27ea7
rafaellang1 <rafael.lang9@gmail.com>
2024-07-30 10:34:13 -0400

    Add: criado custom hooks para erros na pagina de edicao

--- src/hooks/useSafeAsyncState.js
+++ src/hooks/useSafeAsyncState.js
@@ -1,27 +1,18 @@
-import {
-  useEffect, useRef, useState, useCallback,
-} from 'react';
+import { useState, useCallback } from 'react';
+import useIsMounted from './useIsMounted';
 
 // custom hook para resolver o problema de:
 // Can't perform a React state update on an unmounted component
 export default function useSafeAsyncState(initialState) {
   const [state, setState] = useState(initialState);
 
-  const isMounted = useRef(false);
-
-  useEffect(() => {
-    isMounted.current = true;
-
-    return () => {
-      isMounted.current = false;
-    };
-  }, []);
+  const isMounted = useIsMounted();
 
   const setSafeAsyncState = useCallback((data) => {
-    if (isMounted.current) {
+    if (isMounted()) {
       setState(data);
     }
-  }, []);
+  }, [isMounted]);
 
   return [state, setSafeAsyncState];
 }
